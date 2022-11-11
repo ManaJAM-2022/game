6f638c80c8626c1a3427945828aa15e0e6dbf767
Matt Smith <matt.daemon660@gmail.com>
2022-11-11 21:31:56 +0000

    feat: avoid publlic folder
    Moves assets out of the public folder. This is a special folder that directly exports things as-is by vite.
    
    For our case however assets should just be referenced and used.
    
    Additionally this fixes the imports for the other assets by importing and referencing them as expected.
    
    This should reduce our build size if we don't use something.

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,5 +1,10 @@
-import { Direction, GridEngine, GridEngineConfig, Position } from 'grid-engine';
+import { Direction, GridEngine, GridEngineConfig } from 'grid-engine';
 import Phaser from 'phaser';
+import RoomOfficeSpritesheet from '../assets/1_Room_Builder_Office/Room_Builder_Office_16x16.png';
+import ModernOfficeSpritesheet from '../assets/3_Modern_Office_Shadowless/Modern_Office_Shadowless_16x16.png';
+import CharacterAdamSpritesheet from '../assets/Characters_free/Adam_run_16x16.png';
+import route1MP3 from '../audio/route1.mp3';
+import officeMapJSON from '../maps/office.json';
 
 const roomTilesetKey: string = 'base_tiles';
 const officeTilesetKey: string = 'office_tiles';
@@ -8,12 +13,12 @@ const officeTilesetKey: string = 'office_tiles';
 const roomTilesetName: string = 'Room';
 const officeTilesetName: string = 'Office';
 
-const MAP: Record<string, string> = {
-  'office-map': 'maps/office.json',
+const MAP: Record<string, any> = {
+  'office-map': officeMapJSON,
 };
 
 const MUSIC: Record<string, string[]> = {
-  'office-map': ['audio/route1.mp3'],
+  'office-map': [route1MP3],
 };
 
 /**
@@ -32,23 +37,13 @@ export default abstract class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image(
-      roomTilesetKey,
-      'assets/1_Room_Builder_Office/Room_Builder_Office_16x16.png'
-    );
+    this.load.image(roomTilesetKey, RoomOfficeSpritesheet);
 
-    this.load.image(
-      officeTilesetKey,
-      'assets/3_Modern_Office_Shadowless/Modern_Office_Shadowless_16x16.png'
-    );
-    this.load.spritesheet(
-      'player',
-      'assets/Characters_free/Adam_run_16x16.png',
-      {
-        frameWidth: 16,
-        frameHeight: 32,
-      }
-    );
+    this.load.image(officeTilesetKey, ModernOfficeSpritesheet);
+    this.load.spritesheet('player', CharacterAdamSpritesheet, {
+      frameWidth: 16,
+      frameHeight: 32,
+    });
 
     this.load.tilemapTiledJSON(this.mapName, MAP[this.mapName]);
     this.load.audio(`${this.mapName}_Ambience`, MUSIC[this.mapName]);
@@ -61,7 +56,12 @@ export default abstract class GameScene extends Phaser.Scene {
     officeTilemap.addTilesetImage(officeTilesetName, officeTilesetKey);
 
     for (let i = 0; i < officeTilemap.layers.length; i++) {
-      const layer = officeTilemap.createLayer(i, [roomTilesetName, officeTilesetName], 0, 0);
+      const layer = officeTilemap.createLayer(
+        i,
+        [roomTilesetName, officeTilesetName],
+        0,
+        0
+      );
       layer.setDepth(i);
       layer.scale = GameScene.SCALE;
     }
@@ -85,22 +85,32 @@ export default abstract class GameScene extends Phaser.Scene {
             down: { leftFoot: 19, standing: 20, rightFoot: 22 },
           },
           startPosition: { x: 8, y: 8 },
-          labels: [ 'hero' ]
+          labels: ['hero'],
         },
       ],
     };
 
     this.gridEngine?.create(officeTilemap, gridEngineConfig);
 
-    const music = this.sound.add(`${this.mapName}_Ambience`, { loop: true, volume: 0.1 });
+    const music = this.sound.add(`${this.mapName}_Ambience`, {
+      loop: true,
+      volume: 0.1,
+    });
     music.play();
 
-    const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    const spaceBar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
 
     spaceBar.on('down', () => {
       const facingPosition = this.gridEngine?.getFacingPosition('player')!;
 
-      const facingTile = officeTilemap.getTileAt(facingPosition.x, facingPosition.y, true, 2);
+      const facingTile = officeTilemap.getTileAt(
+        facingPosition.x,
+        facingPosition.y,
+        true,
+        2
+      );
       console.log(facingPosition);
       console.log(facingTile);
       if (facingTile && facingTile.properties['interactable']) {
